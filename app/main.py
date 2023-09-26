544c0af993d35c58e42d2676a4c8795316ed8016
Aqude <tovpeko03dev@gmail.com>
2023-09-26 11:42:10 +0300

    основные изменения перед релизом

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 app = FastAPI()
 valid_roles = ["admin", "user", "moderator"]
 @app.get("/users/get_users")
-def get_users(db: Session = Depends(get_db)):
+def __get_users(db: Session = Depends(get_db)):
     users = get_users(db)
     return users
 
@@ -19,15 +19,15 @@ async def __create_user(username: str, role: str = "user", db: Session = Depends
     return addUser
 
 @app.get("/users/{user_id}")
-async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+async def __get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     user = get_user_by_id(db, user_id=user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
 
-@app.get("/users/{role}")
-async def get_users_by_role(role: str, db: Session = Depends(get_db)):
+@app.get("/users/")
+async def __get_users_by_role(role: str = "user", db: Session = Depends(get_db)):
     if role not in valid_roles:
         raise HTTPException(status_code=400, detail=f"Invalid role: {role}. Valid roles are {', '.join(valid_roles)}")
     
--- setup_project.py
+++ setup_project.py
@@ -4,7 +4,7 @@ from app.db.datebase import init_db
 import uvicorn
 
 if __name__ == "__main__":
-    # Создание базы данных
+    # Создание базы данных если её нет.
     init_db()
     # Запуск проекта
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
